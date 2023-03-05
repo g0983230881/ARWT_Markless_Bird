5b7544e984c5597a05b1fca788010ea00ae7a29b
Jordan <g0983230881@gmail.com>
2023-03-05 16:05:14 +0800

    NativeWebSocket_Sample

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebSocket_Example.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
